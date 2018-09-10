1c04d92b3ad94ebf36feb4dba1695c140650470f
anastafj197 <frankie.anastasia@gmail.com>
2018-09-09 20:07:53 -0400

    Updated config file
    Utilizes jasmine2 for pretty output
    Provides failure summary and color scheme

--- conf.js
+++ conf.js
@@ -1,4 +1,6 @@
-// An example configuration file.
+// Frank Anastasia
+// Customized config file with pretty output 
+
 exports.config = {
   directConnect: true,
 
@@ -8,11 +10,39 @@ exports.config = {
   },
 
   // Framework to use. Jasmine is recommended.
-  framework: 'jasmine',
+  framework: 'jasmine2',
+
+  onPrepare: function() {
+    var jasmineReporters = require( 'jasmine-reporters' );
+    jasmine.getEnv().addReporter( new jasmineReporters.JUnitXmlReporter( {
+      consolidateAll: true,
+      savePath:       'testresults',
+      filePrefix:     'xmloutput'
+    } ) );
+
+    var SpecReporter = require( 'jasmine-spec-reporter' ).SpecReporter;
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter( new SpecReporter( {
+      displayStacktrace:      true,
+      displayFailuresSummary: true,
+      displayPendingSummary:  true,
+      displaySuccessfulSpec:  true,
+      displayFailedSpec:      true,
+      displayPendingSpec:     true,
+      displaySpecDuration:    false,
+      displaySuiteNumber:     false,
+      colors: {
+        success: 'green',
+        failure: 'red',
+        pending: 'yellow'
+      },
+      customProcessors: []
+    } ) );
+  },
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['zoo_spec.js'],
+  specs: ['zoo_spec_2.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
@@ -20,3 +50,4 @@ exports.config = {
     defaultTimeoutInterval: 30000
   }
 };
+
