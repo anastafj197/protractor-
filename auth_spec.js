8bae2a47fdec5d3cd21ecddb878f259b0d7ed6ee
anastafj197 <frankie.anastasia@gmail.com>
2018-09-23 13:48:24 -0400

    Automated field entry
    Added an ignore statement for running protractor on a non angular app

--- auth_spec.js
+++ auth_spec.js
@@ -1,9 +1,20 @@
 // End to End test nodeAuth login program 
+browser.ignoreSynchronization = true;
 
 describe("Testing nodeAuth site", function() {
 	
-	it("Should register a user", function() {
+	// Travel to the host adress (localhost:3000)
+	// must have server running to access page 
+	it("Should travel to correct page", function() {
+		browser.get("http://localhost:3000/users/register");
+	}); 
 
+	it("Should register a user", function() {
+		element(by.css('[name="name"]')).sendKeys("Nikki");
+		element(by.css('[name="email"]')).sendKeys("Nikki@gmail.com");
+		element(by.css('[name="username"]')).sendKeys("Snooper");
+		element(by.css('[name="password"]')).sendKeys("777");
+		element(by.css('[name="password2"]')).sendKeys("777");
 	});
 
 	it("Should login with user", function() {
