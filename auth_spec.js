01d39b0d98230351c48b100fd44be0492c0ee24e
anastafj197 <frankie.anastasia@gmail.com>
2018-09-23 11:35:00 -0400

    Create auth_test
    An automated test for my nodeAuth project ensuring 
    1. Registration capabilities 
    2. Login to Members area 
    3. Logout of user

---
+++ auth_spec.js
@@ -0,0 +1,16 @@
+// End to End test nodeAuth login program 
+
+describe("Testing nodeAuth site", function() {
+	
+	it("Should register a user", function() {
+
+	});
+
+	it("Should login with user", function() {
+
+	});
+
+	it("Should logout of user", function() {
+
+	});
+});
