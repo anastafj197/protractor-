39025b60029f36bb022f4198a8033986cfff9739
anastafj197 <frankie.anastasia@gmail.com>
2018-09-23 23:31:53 -0400

    More tests
    Added registration and login test

--- auth_spec.js
+++ auth_spec.js
@@ -1,6 +1,9 @@
 // End to End test nodeAuth login program 
 browser.ignoreSynchronization = true;
 
+var username = "Snooper";
+var password = 777;
+
 describe("Testing nodeAuth site", function() {
 	
 	// Travel to the host adress (localhost:3000)
@@ -10,14 +13,23 @@ describe("Testing nodeAuth site", function() {
 	}); 
 
 	it("Should register a user", function() {
+		// Enter data 
 		element(by.css('[name="name"]')).sendKeys("Nikki");
 		element(by.css('[name="email"]')).sendKeys("Nikki@gmail.com");
-		element(by.css('[name="username"]')).sendKeys("Snooper");
-		element(by.css('[name="password"]')).sendKeys("777");
-		element(by.css('[name="password2"]')).sendKeys("777");
+		element(by.css('[name="username"]')).sendKeys(username);
+		element(by.css('[name="password"]')).sendKeys(password);
+		element(by.css('[name="password2"]')).sendKeys(password);
+
+		// Submit with register button 
+		element(by.css('[type="submit"]')).click()
 	});
 
-	it("Should login with user", function() {
+	it("Should login with chosen username & password", function() {
+		// Enter username / password to login
+		element(by.css('[name="username"]')).sendKeys(username);
+		element(by.css('[name="password"]')).sendKeys(password);
+		// Login with login button 
+		element(by.css('[type="submit"]')).click()
 
 	});
 
